82d6755827eea874caf58ad699b47ab857867672
AbinavRavi <subramathreya@gmail.com>
2022-10-19 21:06:23 +0200

    model not working

--- app/infer.py
+++ app/infer.py
@@ -1,11 +1,24 @@
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
+from pydantic import BaseModel
 from model.inference import inference
 
 app = FastAPI()
 
+class InferRequest(BaseModel):
+    tweet: str
 
-@app.get('/', status_code=status.HTTP_200_OK)
-def run_inference(req: str):
-    prediction = inference(req)
-    return req, prediction
+
+@app.post('/infer', status_code=status.HTTP_200_OK)
+def run_inference(infer_request: InferRequest):
+    text = infer_request.tweet
+    prediction = inference(text)
+    if prediction == 0:
+        pred = "elon"
+    else:
+        pred = "kanye"
+    response = {
+        "tweet" : text,
+        "prediction" : pred
+    }
+    return response
